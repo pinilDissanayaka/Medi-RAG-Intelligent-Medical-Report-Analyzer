e386e3899e6434289bb2c14806b9ac5a40b495d7
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-11-02 19:30:03 +0530

    add home page

---
+++ home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="Medi-RAG-Intelligent-Medical-Report-Analyzer", page_icon="🩺", layout="centered")
+
+
+st.title("🩺 Medi-RAG: AI-powered Medical Report Analysis 🧠")
+
+st.image(image="assets/medi-rag.jpg", width=500, caption="Medi-RAG-Intelligent-Medical-Report-Analyzer")
+
+st.subheader("""🌐 Unlock the future of healthcare with Medi-RAG, a 
+             revolutionary tool that leverages AI 🤖 to analyze and summarize complex medical reports 
+             with precision. 
+             Designed to support doctors, researchers, and healthcare professionals, 
+             Medi-RAG combines powerful natural language understanding 📖 with advanced medical data 
+             retrieval 📊.""")
+
+st.subheader("🔍 Key Features:")
