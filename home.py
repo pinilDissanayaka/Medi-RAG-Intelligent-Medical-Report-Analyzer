5dc9dfacfc0fee202ace313e20fdbea40047505a
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-11-02 22:54:35 +0530

    done home page

--- home.py
+++ home.py
@@ -1,22 +1,26 @@
 import streamlit as st
 
-st.set_page_config(page_title="Medi-RAG-Intelligent-Medical-Report-Analyzer", page_icon="🩺", layout="centered")
+st.set_page_config(page_title="Medi-RAG-Intelligent-Medical-Report-Analyzer", 
+                   page_icon="🩺", 
+                   layout="centered")
 
 
 st.title("🩺 Medi-RAG: AI-powered Medical Report Analysis 🧠")
 
 st.image(image="assets/medi-rag.jpg", caption="Medi-RAG-Intelligent-Medical-Report-Analyzer")
 
-st.text("🌐 Unlock the future of healthcare with Medi-RAG, a revolutionary tool that leverages AI 🤖 to analyze and summarize complex medical reports with precision.Designed to support doctors, researchers, and healthcare professionals, Medi-RAG combines powerful natural language understanding 📖 with advanced medical data retrieval 📊.")
+st.markdown("🌐 Unlock the future of healthcare with Medi-RAG, a revolutionary tool that leverages AI 🤖 to analyze and summarize complex medical reports with precision.Designed to support doctors, researchers, and healthcare professionals, Medi-RAG combines powerful natural language understanding 📖 with advanced medical data retrieval 📊.")
 
 st.subheader("🔍 Key Features:")
 
-st.text("Insightful Summaries 📑: Get quick, clear highlights from lengthy reports.")
+st.markdown("""
+            - Insightful Summaries 📑: Get quick, clear highlights from lengthy reports.
+            
+            - Condition Contextualization 🧬: Access relevant medical insights instantly.
+            
+            - Anomaly Detection 🚨: Identify unusual findings and potential concerns.
+            
+            - Real-Time Q&A ❓: Ask specific questions and receive accurate answers from trusted medical sources.
+            """)
 
-st.text("Condition Contextualization 🧬: Access relevant medical insights instantly.")
-
-st.text("Anomaly Detection 🚨: Identify unusual findings and potential concerns.")
-
-st.text("Real-Time Q&A ❓: Ask specific questions and receive accurate answers from trusted medical sources.")
-
-st.header("⚕️ Join the new wave of healthcare, where human expertise meets AI-driven intelligence for better, faster, and more reliable patient care! 🌟")
+st.subheader("⚕️ Join the new wave of healthcare, where human expertise meets AI-driven intelligence for better, faster, and more reliable patient care! 🌟")
