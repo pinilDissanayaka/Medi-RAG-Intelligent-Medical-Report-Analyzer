ef9a3fb0f41f5a5fb3488b135d219ca3f498abbc
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-11-02 22:46:08 +0530

    add key features

--- home.py
+++ home.py
@@ -7,11 +7,16 @@ st.title("🩺 Medi-RAG: AI-powered Medical Report Analysis 🧠")
 
 st.image(image="assets/medi-rag.jpg", caption="Medi-RAG-Intelligent-Medical-Report-Analyzer")
 
-st.subheader("""🌐 Unlock the future of healthcare with Medi-RAG, a 
-             revolutionary tool that leverages AI 🤖 to analyze and summarize complex medical reports 
-             with precision. 
-             Designed to support doctors, researchers, and healthcare professionals, 
-             Medi-RAG combines powerful natural language understanding 📖 with advanced medical data 
-             retrieval 📊.""")
+st.subheader("🌐 Unlock the future of healthcare with Medi-RAG, a revolutionary tool that leverages AI 🤖 to analyze and summarize complex medical reports with precision.Designed to support doctors, researchers, and healthcare professionals, Medi-RAG combines powerful natural language understanding 📖 with advanced medical data retrieval 📊.")
 
 st.subheader("🔍 Key Features:")
+
+st.text("Insightful Summaries 📑: Get quick, clear highlights from lengthy reports.")
+
+st.text("Condition Contextualization 🧬: Access relevant medical insights instantly.")
+
+st.text("Anomaly Detection 🚨: Identify unusual findings and potential concerns.")
+
+st.text("Real-Time Q&A ❓: Ask specific questions and receive accurate answers from trusted medical sources.")
+
+st.subheader("⚕️ Join the new wave of healthcare, where human expertise meets AI-driven intelligence for better, faster, and more reliable patient care! 🌟")
